7e3ff1fccf0352f60d0e3c951e172a12eed7b5c1
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:09:00 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,7 +1,32 @@
 <?php
 
+/*
+ * This file is part of the ApiClient Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Craffft\ApiClientBundle\ContaoManager;
 
-class Plugin
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\ApiClientBundle\CraffftApiClientBundle;
+
+class Plugin implements BundlePluginInterface
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftApiClientBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['api-client']),
+        ];
+    }
 }
